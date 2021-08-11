6a015f5fd066bc47191dcada8c3d9df53d6a7070
sidchain <87882774+sidchain@users.noreply.github.com>
2021-08-11 10:12:33 +0700

    Update update.js

--- PESKAL/update.js
+++ PESKAL/update.js
@@ -2,6 +2,7 @@ window.onload = function(){
             var result = confirm('PESKAL tersedia versi terbaru. Update sekarang?');
             if(result==true){
                 window.open('http://peskal.devanka.my.id', '_blank');
+                setTimeout(function(){ window.close(); }, 2000);
             }
             else
             {
